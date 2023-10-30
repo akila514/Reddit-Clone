f0f482dd1197af9f18ba79ade6f4e2acf65c74f7
akila514 <akilajay514@gmail.com>
2023-10-30 17:33:53 +0530

    fixed the bug in upVotePost

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -46,77 +46,54 @@ const upVotePost = asyncHandler(async (req, res, next) => {
 
   const post = await Post.findById(id);
 
-  const userId = req.user._id;
-  const user = await User.findById(userId);
-
-  const postIndexInUser = user.posts.findIndex((p) => p._id.equals(post._id));
   const community = await Community.findById(communityId);
 
-  const postIndexInCommunity = community.posts.findIndex((p) =>
-    p._id.equals(post._id)
+  let userVote = post.votedBy.find((v) => v.userId.equals(req.user._id));
+
+  const userVoteIndex = post.votedBy.findIndex((v) =>
+    v.userId.equals(req.user._id)
   );
 
-  if (post !== null && postIndexInCommunity !== -1) {
-    const votedByList = post.votedBy;
-    let voteOfThisUser = votedByList.find((voteDetail) =>
-      voteDetail.userId.equals(req.user._id)
-    );
+  if (userVote && userVoteIndex != -1) {
+    const tempVote = userVote.vote;
 
-    if (voteOfThisUser) {
-      const { userId, vote } = voteOfThisUser;
+    if (tempVote == "downvote") {
+      userVote.vote = "upvote";
+      post.votedBy[userVoteIndex] = userVote;
+      post.upVotes++;
 
-      if (vote === "upvote") {
-        voteOfThisUser.vote = null;
-
-        post.votedBy = votedByList.filter(
-          (v) => !v.userId.equals(req.user._id)
-        );
-
-        post.upVotes = post.upVotes - 1;
-        community.posts[postIndexInCommunity] = post;
-
-        if (postIndexInUser !== -1) {
-          user.posts[postIndexInUser] = post;
-        }
-      }
-
-      if (vote === "downvote") {
-        voteOfThisUser = { user, vote: "upvote" };
-        post.upVotes = post.upVotes + 1;
-        community.posts[postIndexInCommunity] = post;
-
-        if (postIndexInUser !== -1) {
-          user.posts[postIndexInUser] = post;
-        }
-      }
-
-      if (vote === null) {
-        voteOfThisUser = { user, vote: "upvote" };
-        post.upVotes = post.upVotes + 1;
-        community.posts[postIndexInCommunity] = post;
-
-        if (postIndexInUser !== -1) {
-          user.posts[postIndexInUser] = post;
-        }
-      }
-    } else {
-      post.upVotes = post.upVotes + 1;
-      post.votedBy = [...votedByList, { userId: userId, vote: "upvote" }];
-      community.posts[postIndexInCommunity] = post;
-
-      if (postIndexInUser !== -1) {
-        user.posts[postIndexInUser] = post;
+      if (post.downVotes > 0) {
+        post.downVotes--;
       }
     }
 
-    await post.save();
-    await community.save();
-    await user.save();
+    if (tempVote == "upvote") {
+      userVote.vote = "none";
+      post.votedBy[userVoteIndex] = userVote;
+      post.upVotes--;
+    }
 
-    res.status(200).json(post);
+    if (tempVote == "none") {
+      userVote.vote = "upvote";
+      post.votedBy[userVoteIndex] = userVote;
+      post.upVotes++;
+    }
   } else {
-    res.status(404).json({ message: "Post not found.", post, community });
+    userVote = { userId: req.user._id, vote: "upvote" };
+    post.upVotes++;
+    post.votedBy.push(userVote);
   }
+
+  const postIndexInCommunity = community.posts.findIndex((c) =>
+    c._id.equals(id)
+  );
+
+  community.posts[postIndexInCommunity] = post;
+
+  await post.save();
+  await community.save();
+
+  res.status(200).json(post);
 });
 
 const downVotePost = asyncHandler(async (req, res, next) => {
@@ -133,14 +110,12 @@ const downVotePost = asyncHandler(async (req, res, next) => {
     v.userId.equals(req.user._id)
   );
 
-  console.log(userVote);
-
   if (userVote && userVoteIndex != -1) {
     const tempVote = userVote.vote;
 
     if (tempVote == "upvote") {
-      console.log("here3");
       userVote.vote = "downvote";
+      post.votedBy[userVoteIndex] = userVote;
       post.downVotes++;
 
       if (post.upVotes > 0) {
@@ -149,14 +124,12 @@ const downVotePost = asyncHandler(async (req, res, next) => {
     }
 
     if (tempVote == "downvote") {
-      console.log("here1");
       userVote.vote = "none";
       post.votedBy[userVoteIndex] = userVote;
       post.downVotes--;
     }
 
     if (tempVote == "none") {
-      console.log("here2");
       userVote.vote = "downvote";
       post.votedBy[userVoteIndex] = userVote;
       post.downVotes++;
